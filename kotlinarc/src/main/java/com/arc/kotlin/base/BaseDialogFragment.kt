c70d1afa9a4423cbb74d8d52abe05e3a295a2130
MOHAN <mohan.raj@mobiotics.com>
2019-06-03 10:51:17 +0530

    Code changes.
    Signed-off-by: MOHAN <mohan.raj@mobiotics.com>

--- kotlinarc/src/main/java/com/arc/kotlin/base/BaseDialogFragment.kt
+++ kotlinarc/src/main/java/com/arc/kotlin/base/BaseDialogFragment.kt
@@ -2,20 +2,27 @@ package com.arc.kotlin.base
 
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import dagger.Lazy
 import javax.inject.Inject
 
-/**
- * MOHAN on 28-10-2017.
- */
 abstract class BaseDialogFragment<P : BasePresenter<*>> : DialogFragment() {
 
     @Inject
-    lateinit var presenter: P
+    lateinit var presenter: Lazy<P>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inject()
     }
 
+    protected fun presenter(): P {
+        return presenter.get()
+    }
+
     protected abstract fun inject()
+
+    override fun onDestroyView() {
+        presenter().detach()
+        super.onDestroyView()
+    }
 }
--- kotlinarc/src/main/java/com/arc/kotlin/base/BaseFragment.kt
+++ kotlinarc/src/main/java/com/arc/kotlin/base/BaseFragment.kt
@@ -2,18 +2,27 @@ package com.arc.kotlin.base
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import dagger.Lazy
 import javax.inject.Inject
 
-/** MOHAN on 28-10-2017.  */
 abstract class BaseFragment<P : BasePresenter<*>> : Fragment() {
 
     @Inject
-    lateinit var presenter: P
+    lateinit var presenter: Lazy<P>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inject()
     }
 
+    protected fun presenter(): P {
+        return presenter.get()
+    }
+
     protected abstract fun inject()
+
+    override fun onDestroyView() {
+        presenter().detach()
+        super.onDestroyView()
+    }
 }
